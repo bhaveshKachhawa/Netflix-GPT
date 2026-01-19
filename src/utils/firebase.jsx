7bfc33d7e67d8af2aaece0bb2c4a67eead53a6b3
Bhavesh Kachhawa <kachhawabhavesh27@gmail.com>
2026-01-19 11:08:26 +0530

    update gitignore and firbase file

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -8,13 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGsnPJEB4HG66K28yOhcajF70hvnIzMI8",
-  authDomain: "netflixgpt-8abfd.firebaseapp.com",
-  projectId: "netflixgpt-8abfd",
-  storageBucket: "netflixgpt-8abfd.firebasestorage.app",
-  messagingSenderId: "810746096836",
-  appId: "1:810746096836:web:40519f4f619682d3126d91",
-  measurementId: "G-ZLGKNDC7N0"
+apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
